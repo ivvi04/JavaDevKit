fb1103e0384fcebf43d5f78237a3dee61a280bf1
LakeevDA <ivvi04@gmail.com>
2023-11-29 15:58:01 +0300

    Добавлено домашние задания из урока 2

--- src/main/java/ru/lakeevda/lesson2/circles/bricks/MainWindow.java
+++ src/main/java/ru/lakeevda/lesson2/circles/bricks/MainWindow.java
@@ -1,5 +1,6 @@
 package ru.lakeevda.lesson2.circles.bricks;
 
+import ru.lakeevda.lesson2.circles.circles.Ball;
 import ru.lakeevda.lesson2.circles.common.Background;
 import ru.lakeevda.lesson2.circles.common.CanvasRepaintListener;
 import ru.lakeevda.lesson2.circles.common.Interactable;
@@ -7,31 +8,35 @@ import ru.lakeevda.lesson2.circles.common.MainCanvas;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainWindow extends JFrame implements CanvasRepaintListener {
     private static final int POS_X = 200;
     private static final int POS_Y = 200;
     private static final int WINDOW_WIDTH = 600;
     private static final int WINDOW_HEIGHT = 600;
-    private final Interactable[] interactables = new Interactable[10];
+    private final java.util.List<Interactable> interactables = new ArrayList<>();
+    private final List<Background> backgrounds = new ArrayList<>();
 
     private MainWindow() {
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setBounds(POS_X, POS_Y, WINDOW_WIDTH, WINDOW_HEIGHT);
         setTitle("Bricks");
-        interactables[0] = new Background() {
-            @Override
-            public void update(MainCanvas mainCanvas, float deltaTime) {
-                super.update(mainCanvas, deltaTime);
-            }
+        backgrounds.add(new Background() {
+                            @Override
+                            public void update(MainCanvas mainCanvas, float deltaTime) {
+                                super.update(mainCanvas, deltaTime);
+                            }
 
-            @Override
-            public void render(MainCanvas mainCanvas, Graphics graphics) {
-                super.render(mainCanvas, graphics);
-            }
-        };
-        for (int i = 1; i < interactables.length; i++) {
-            interactables[i] = new Brick();
+                            @Override
+                            public void render(MainCanvas mainCanvas, Graphics graphics) {
+                                super.render(mainCanvas, graphics);
+                            }
+                        }
+        );
+        for (int i = 0; i < 10; i++) {
+            interactables.add(new Brick());
         }
 
 
@@ -47,16 +52,20 @@ public class MainWindow extends JFrame implements CanvasRepaintListener {
     }
 
     private void update(MainCanvas mainCanvas, float deltaTime) {
-//        interactables[1].update(mainCanvas, deltaTime);
-        for (int i = 0; i < interactables.length; i++) {
-            interactables[i].update(mainCanvas, deltaTime);
+        for (Background background : backgrounds) {
+            background.update(mainCanvas, deltaTime);
+        }
+        for (Interactable interactable : interactables) {
+            interactable.update(mainCanvas, deltaTime);
         }
     }
 
     private void render(MainCanvas mainCanvas, Graphics graphics) {
-//        interactables[1].render(mainCanvas, graphics);
-        for (int i = 0; i < interactables.length; i++) {
-            interactables[i].render(mainCanvas, graphics);
+        for (Background background : backgrounds) {
+            background.render(mainCanvas, graphics);
+        }
+        for (Interactable interactable : interactables) {
+            interactable.render(mainCanvas, graphics);
         }
     }
 
